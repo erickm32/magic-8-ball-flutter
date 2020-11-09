97d78e3ea7797f8393397bb7467828671107a19a
Érick Moreira <erick@sementesrastreadas.com.br>
2020-11-09 18:10:20 -0300

    Finishing the idea of the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MaterialApp(
+      home: BallPage(),
+    ));
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Ball(),
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
       ),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = randomBallNumber();
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: refreshMessage,
+      ),
+    );
+  }
+
+  static int randomBallNumber() {
+    return Random().nextInt(5) + 1;
+  }
+
+  refreshMessage() {
+    setState(() {
+      ballNumber = randomBallNumber();
+    });
+  }
+}
